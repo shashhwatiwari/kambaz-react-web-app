29b8fceb291db25e26f7ae9f94666df6a1778242
Shashwat Tiwari <106163122+shashhwatiwari@users.noreply.github.com>
2025-07-16 01:08:25 +0530

    slight alignment fix and missed button added

--- src/Kambaz/Courses/Assignments/AssignmentControls.tsx
+++ src/Kambaz/Courses/Assignments/AssignmentControls.tsx
@@ -4,26 +4,57 @@ import { Form } from 'react-bootstrap';
 import { CiSearch } from "react-icons/ci";
 export default function AssignmentControls() {
     return (
-        <div id="wd-assignments-controls" className="text-nowrap">
-            <InputGroup className="me-1 float-start w-50 w-md-25" style={{ width: "40%" }}>
+        <div id="wd-assignments-controls"
+             className="d-flex justify-content-between align-items-center"
+        style={{
+         marginLeft: "-1rem",
+         marginRight: "-1rem",
+         paddingLeft: "1rem",
+         paddingRight: "1rem"
+     }}>
+            <InputGroup className="me-1" style={{width: "40%"}}>
                 <InputGroup.Text>
-                    <CiSearch />
+                    <CiSearch/>
                 </InputGroup.Text>
-                <Form.Control className=""
+                <Form.Control
                     type="search"
                     placeholder="Search..."
                     size="lg"
                 />
             </InputGroup>
-            <Button variant="danger" size="lg" className="me-1 float-end" id="wd-add-assignment-btn">
-                <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
-                Assignment
-            </Button>
-            {/* Add more controls as needed */}
-            <Button variant="secondary" size="lg" className="me-1 float-end" id="wd-add-assignment-group-btn">
-                <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
-                Group
-            </Button>
+
+            <div>
+                <Button variant="secondary" size="lg" className="me-1"
+                        id="wd-add-assignment-group-btn">
+                    <FaPlus className="position-relative me-2" style={{bottom: "1px"}}/>
+                    Group
+                </Button>
+                <Button variant="danger" size="lg" className="me-1" id="wd-add-assignment-btn">
+                    <FaPlus className="position-relative me-2" style={{bottom: "1px"}}/>
+                    Assignment
+                </Button>
+            </div>
         </div>
+        // <div id="wd-assignments-controls" className="text-nowrap">
+        //     <InputGroup className="me-1 float-start w-50 w-md-25" style={{ width: "40%" }}>
+        //         <InputGroup.Text>
+        //             <CiSearch />
+        //         </InputGroup.Text>
+        //         <Form.Control className=""
+        //             type="search"
+        //             placeholder="Search..."
+        //             size="lg"
+        //         />
+        //     </InputGroup>
+        //     <Button variant="danger" size="lg" className="me-1 float-end" id="wd-add-assignment-btn">
+        //         <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
+        //         Assignment
+        //     </Button>
+        //     {/* Add more controls as needed */}
+        //     <Button variant="secondary" size="lg" className="me-1 float-end" id="wd-add-assignment-group-btn">
+        //         <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
+        //         Group
+        //     </Button>
+        // </div>
     );
 }
--- src/Kambaz/Courses/Navigation.tsx
+++ src/Kambaz/Courses/Navigation.tsx
@@ -1,20 +1,24 @@
 import { Link } from "react-router-dom";
 export default function CourseNavigation() {
   return (
-    <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      <Link to="/Kambaz/Courses/1234/Home" id="wd-course-home-link"
-        className="list-group-item active border border-0"> Home </Link>
-      <Link to="/Kambaz/Courses/1234/Modules" id="wd-course-modules-link"
-        className="list-group-item text-danger border border-0"> Modules </Link>
-      <Link to="/Kambaz/Courses/1234/Piazza" id="wd-course-piazza-link"
-        className="list-group-item text-danger border border-0"> Piazza </Link>
-      <Link to="/Kambaz/Courses/1234/Zoom" id="wd-course-zoom-link"
-        className="list-group-item text-danger border border-0"> Zoom </Link>
-      <Link to="/Kambaz/Courses/1234/Assignments" id="wd-course-quizzes-link"
-        className="list-group-item text-danger border border-0"> Assignments </Link>
-      <Link to="/Kambaz/Courses/1234/Quizzes" id="wd-course-assignments-link"
-        className="list-group-item text-danger border border-0"> Quizzes </Link>
-      <Link to="/Kambaz/Courses/1234/People" id="wd-course-people-link"
-        className="list-group-item text-danger border border-0" > People </Link>
-    </div>
-);}
+      <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
+          <Link to="/Kambaz/Courses/1234/Home" id="wd-course-home-link"
+                className="list-group-item active border border-0"> Home </Link>
+          <Link to="/Kambaz/Courses/1234/Modules" id="wd-course-modules-link"
+                className="list-group-item text-danger border border-0"> Modules </Link>
+          <a href="https://piazza.com/class/mchnh9kzzbu35p" target="_blank"
+             id="wd-course-piazza-link"
+             className="list-group-item text-danger border border-0">Piazza</a>
+          <a href="https://zoom.us/myhome" target="_blank" id="wd-course-zoom-link"
+             className="list-group-item text-danger border border-0">Zoom</a>
+          <Link to="/Kambaz/Courses/1234/Assignments" id="wd-course-quizzes-link"
+                className="list-group-item text-danger border border-0"> Assignments </Link>
+          <Link to="/Kambaz/Courses/1234/Quizzes" id="wd-course-assignments-link"
+                className="list-group-item text-danger border border-0"> Quizzes </Link>
+          <Link to="/Kambaz/Courses/1234/Grades" id="wd-course-grades-link"
+                className="list-group-item text-danger border border-0">Grades</Link>
+          <Link to="/Kambaz/Courses/1234/People" id="wd-course-people-link"
+                className="list-group-item text-danger border border-0"> People </Link>
+      </div>
+  );
+}
