aa4638dc08c87d6c453bdd52094b01b47901d830
Shashwat Tiwari <106163122+shashhwatiwari@users.noreply.github.com>
2025-07-23 01:17:24 +0530

    A3 work - 1. assignments.json added to make it data driven and not static all across the courses 2. users.json added for data driven users and to dynamically associate them to courses through id matching 3. enrollments.json added to associate users with courses 4. Assignment/editor.tsx updated to take in assignment description, timings and scores dynamically from the data instead of being hard coded (New 22nd July addition) 5. Sign up page navigation bar now displays active tab

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -1,10 +1,23 @@
-import "../styles.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 export default function AccountNavigation() {
+  const { pathname } = useLocation();
+  const links = ["Signin", "Signup", "Profile"];
+
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-      <Link to={'/Kambaz/Account/Signin'}  className="list-group-item active border border-0"> Signin  </Link>
-      <Link to={'/Kambaz/Account/Signup'}  className="list-group-item text-danger border border-0"> Signup  </Link>
-      <Link to={'/Kambaz/Account/Profile'} className="list-group-item text-danger border border-0"> Profile </Link>
+      {links.map((link) => {
+        const linkPath = `/Kambaz/Account/${link}`;
+        const isActive = pathname === linkPath;
+        return (
+          <Link
+            key={link}
+            to={linkPath}
+            className={`list-group-item border border-0 ${isActive ? "active text-dark" : "text-danger"}`}
+          >
+            {link}
+          </Link>
+        );
+      })}
     </div>
-);}
+  );
+}
