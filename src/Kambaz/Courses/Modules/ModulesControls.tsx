1ebc173fecbf505ed676cb36155a6145d8dc80b4
Shashwat Tiwari <106163122+shashhwatiwari@users.noreply.github.com>
2025-07-28 02:34:05 +0530

    Changed visibility for control buttons on module and assignment pages to be aesthetically more in line with actual Canvas

--- src/Kambaz/Courses/Modules/ModulesControls.tsx
+++ src/Kambaz/Courses/Modules/ModulesControls.tsx
@@ -63,11 +63,13 @@ export default function ModulesControls(
   {
     moduleName,
     setModuleName,
-    addModule
+    addModule,
+    isFaculty = true  // Add this prop with default true
   }: {
     moduleName: string;
     setModuleName: (title: string) => void;
     addModule: () => void;
+    isFaculty?: boolean;  // Add this type
   }
 ) {
   const [show, setShow] = useState(false);
@@ -77,36 +79,40 @@ export default function ModulesControls(
   return (
     <>
       <div id="wd-modules-controls" className="text-nowrap">
-        <Button
-          variant="danger"
-          onClick={handleShow}
-          size="lg"
-          className="me-1 float-end"
-          id="wd-add-module-btn"
-        >
-          <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
-          Module
-        </Button>
+        {isFaculty && (
+          <>
+            <Button
+              variant="danger"
+              onClick={handleShow}
+              size="lg"
+              className="me-1 float-end"
+              id="wd-add-module-btn"
+            >
+              <FaPlus className="position-relative me-2" style={{ bottom: "1px" }} />
+              Module
+            </Button>
 
-        <Dropdown className="float-end me-2">
-          <Dropdown.Toggle variant="secondary" size="lg" id="wd-publish-all-btn">
-            <GreenCheckmark /> Publish All
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.Item id="wd-publish-all-modules-and-items">
-              <GreenCheckmark /> Publish all modules and items
-            </Dropdown.Item>
-            <Dropdown.Item id="wd-publish-modules-only">
-              <GreenCheckmark /> Publish modules only
-            </Dropdown.Item>
-            <Dropdown.Item id="wd-unpublish-all-modules-and-items">
-              <MdDoNotDisturbAlt className="me-1 fs-5" /> Unpublish all modules and items
-            </Dropdown.Item>
-            <Dropdown.Item id="wd-unpublish-modules-only">
-              <MdDoNotDisturbAlt className="me-1 fs-5" /> Unpublish modules only
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
+            <Dropdown className="float-end me-2">
+              <Dropdown.Toggle variant="secondary" size="lg" id="wd-publish-all-btn">
+                <GreenCheckmark /> Publish All
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item id="wd-publish-all-modules-and-items">
+                  <GreenCheckmark /> Publish all modules and items
+                </Dropdown.Item>
+                <Dropdown.Item id="wd-publish-modules-only">
+                  <GreenCheckmark /> Publish modules only
+                </Dropdown.Item>
+                <Dropdown.Item id="wd-unpublish-all-modules-and-items">
+                  <MdDoNotDisturbAlt className="me-1 fs-5" /> Unpublish all modules and items
+                </Dropdown.Item>
+                <Dropdown.Item id="wd-unpublish-modules-only">
+                  <MdDoNotDisturbAlt className="me-1 fs-5" /> Unpublish modules only
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </>
+        )}
 
         <Button
           variant="secondary"
@@ -126,14 +132,16 @@ export default function ModulesControls(
         </Button>
       </div>
 
-      <ModuleEditor
-        show={show}
-        handleClose={handleClose}
-        dialogTitle="Add Module"
-        moduleName={moduleName}
-        setModuleName={setModuleName}
-        addModule={addModule}
-      />
+      {isFaculty && (
+        <ModuleEditor
+          show={show}
+          handleClose={handleClose}
+          dialogTitle="Add Module"
+          moduleName={moduleName}
+          setModuleName={setModuleName}
+          addModule={addModule}
+        />
+      )}
     </>
   );
 }
--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -1,3 +1,95 @@
+// import { FormControl, ListGroup } from "react-bootstrap";
+// import ModulesControls from "./ModulesControls";
+// import { BsGripVertical } from "react-icons/bs";
+// import ModuleControlButtons from "./ModuleControlButtons";
+// import LessonControlButtons from "./LessonControlButtons";
+// import { useParams } from "react-router";
+// import { useState } from "react";
+// import { addModule, editModule, updateModule, deleteModule } from "./reducer";
+// import { useSelector, useDispatch } from "react-redux";
+//
+// export default function Modules() {
+//   const { cid } = useParams();
+//   const [moduleName, setModuleName] = useState("");
+//   const { modules } = useSelector((state: any) => state.modulesReducer);
+//   const dispatch = useDispatch();
+//
+//   const currentUser = useSelector((state: any) => state.accountReducer?.currentUser);
+//   const isFaculty = currentUser?.role === "FACULTY";
+//
+//   return (
+//     <div className="me-4">
+//       {/* showing the controls  when user is faculty */}
+//       {isFaculty && (
+//         <>
+//           <ModulesControls
+//             setModuleName={setModuleName}
+//             moduleName={moduleName}
+//             addModule={() => {
+//               dispatch(addModule({ name: moduleName, course: cid }));
+//               setModuleName("");
+//             }}
+//           />
+//           <br /><br /><br /><br />
+//         </>
+//       )}
+//
+//       <ListGroup id="wd-modules" className="rounded-0">
+//         {modules
+//           .filter((module: any) => module.course === cid)
+//           .map((module: any) => (
+//             <ListGroup.Item
+//               key={module._id}
+//               className="wd-module p-0 mb-5 fs-5 border-gray"
+//             >
+//               <div className="wd-title p-3 ps-2 bg-secondary d-flex justify-content-between align-items-center">
+//                 <div className="d-flex align-items-center">
+//                   <BsGripVertical className="me-2 fs-3" />
+//
+//                   {!module.editing && <span>{module.name}</span>}
+//                   {module.editing && isFaculty && (
+//                     <FormControl
+//                       className="w-50 d-inline-block"
+//                       defaultValue={module.name}
+//                       onChange={(e) =>
+//                         dispatch(updateModule({ ...module, name: e.target.value }))
+//                       }
+//                       onKeyDown={(e) => {
+//                         if (e.key === "Enter") {
+//                           dispatch(updateModule({ ...module, editing: false }));
+//                         }
+//                       }}
+//                     />
+//                   )}
+//                 </div>
+//
+//                 {isFaculty && (
+//                   <ModuleControlButtons
+//                     moduleId={module._id}
+//                     deleteModule={(moduleId) => dispatch(deleteModule(moduleId))}
+//                     editModule={(moduleId) => dispatch(editModule(moduleId))}
+//                   />
+//                 )}
+//               </div>
+//
+//               {module.lessons && (
+//                 <ListGroup className="wd-lessons rounded-0">
+//                   {module.lessons.map((lesson: any) => (
+//                     <ListGroup.Item key={lesson._id} className="wd-lesson p-3 ps-1">
+//                       <BsGripVertical className="me-2 fs-3" />
+//                       {lesson.name}
+//                       {/* Only show LessonControlButtons if user is FACULTY */}
+//                       {isFaculty && <LessonControlButtons />}
+//                     </ListGroup.Item>
+//                   ))}
+//                 </ListGroup>
+//               )}
+//             </ListGroup.Item>
+//           ))}
+//       </ListGroup>
+//     </div>
+//   );
+// }
 import { FormControl, ListGroup } from "react-bootstrap";
 import ModulesControls from "./ModulesControls";
 import { BsGripVertical } from "react-icons/bs";
@@ -19,20 +111,16 @@ export default function Modules() {
 
   return (
     <div className="me-4">
-      {/* showing the controls  when user is faculty */}
-      {isFaculty && (
-        <>
-          <ModulesControls
-            setModuleName={setModuleName}
-            moduleName={moduleName}
-            addModule={() => {
-              dispatch(addModule({ name: moduleName, course: cid }));
-              setModuleName("");
-            }}
-          />
-          <br /><br /><br /><br />
-        </>
-      )}
+      <ModulesControls
+        setModuleName={setModuleName}
+        moduleName={moduleName}
+        addModule={() => {
+          dispatch(addModule({ name: moduleName, course: cid }));
+          setModuleName("");
+        }}
+        isFaculty={isFaculty}
+      />
+      <br /><br /><br /><br />
 
       <ListGroup id="wd-modules" className="rounded-0">
         {modules
